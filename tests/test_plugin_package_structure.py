dc33452fc9172884b82af343df85f608439d5a4e
Christian Cabello <christian.cabello@ip-paris.fr>
2025-06-07 19:14:50 +0200

    Update tests and build config to newest template

--- tests/test_plugin_package_structure.py
+++ tests/test_plugin_package_structure.py
@@ -8,9 +8,6 @@ import pytest
 from pathlib import Path
 import importlib
 import pkgutil
-from collections.abc import Iterable
-
-from pymodaq_data import Q_, Unit
 
 
 MANDATORY_MOVE_METHODS = ['ini_attributes', 'get_actuator_value', 'close', 'commit_settings',
@@ -86,24 +83,6 @@ def test_move_has_mandatory_methods():
             assert hasattr(klass, meth)
 
 
-def test_move_has_correct_units():
-    plugin_list, move_mod = get_move_plugins()
-    for plug in plugin_list:
-        name = plug.split('daq_move_')[1]
-        klass = getattr(getattr(move_mod, plug), f'DAQ_Move_{name}')
-        if not isinstance(klass._controller_units, list):
-            if isinstance(klass._controller_units, dict):
-                units = list(klass._controller_units.values())
-            elif isinstance(klass._controller_units, str):
-                units = [klass._controller_units]
-            else:
-                raise TypeError(f'{klass._controller_units} is an invalid type')
-        else:
-            units = klass._controller_units
-        for unit in units:
-            Unit(unit)  # check if the unit is known from pint
-
-
 @pytest.mark.parametrize('dim', ('0D', '1D', '2D', 'ND'))
 def test_viewer_has_mandatory_methods(dim):
     plugin_list, mod = get_viewer_plugins(dim)
@@ -116,19 +95,3 @@ def test_viewer_has_mandatory_methods(dim):
         klass = getattr(module, f'DAQ_{dim}Viewer_{name}')
         for meth in MANDATORY_VIEWER_METHODS:
             assert hasattr(klass, meth)
-
-def test_compatibility(capsys):
-    capsys.disabled()
-    try:
-        from pymodaq_plugin_manager.compatibility_checker import PyMoDAQPlugin
-    except (ModuleNotFoundError, ImportError) as e:
-        pytest.fail(f"Please update pymodaq_plugin_manager to a newer version: {e}")
-
-    plugin = PyMoDAQPlugin(get_package_name(), None)
-    success = plugin.all_imports_valid()
-    msg = '\n'.join(plugin._failed_imports + [''])
-
-    if not success:
-        plugin.save_import_report(".")
-
-    assert success, msg
